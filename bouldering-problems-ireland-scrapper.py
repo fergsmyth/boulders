9d8b5e954a9da35758651f53c9be0fc5127c1067
Ferg Smyth <ferghal.smyth@zalando.ie>
2020-07-01 20:46:54 +0100

    Adding scapper

---
+++ bouldering-problems-ireland-scrapper.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+from tabulate import tabulate
+from sqlalchemy import create_engine
+engine = create_engine('postgresql://postgres:mysecretpassword@localhost:5432/postgres')
+
+problemsUrl = 'http://www.theshortspan.com/newproblemdatabase/newproblemdatabase.php'
+page = requests.get(problemsUrl)
+soup = BeautifulSoup(page.content, "html5lib")
+problemsTable = soup.find('table', {'width':'100%'})
+df = pd.read_html(str(problemsTable))[0]
+df.columns = df.iloc[0]
+df = df.drop(df.index[0])
+df = df.apply(lambda x: x.astype(str).str.lower())
+df.to_sql('problems', engine, if_exists='replace')
