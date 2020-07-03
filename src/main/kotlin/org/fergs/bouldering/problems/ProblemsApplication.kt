0775ba5a34f1e4105e44f69a62e930c5cf6df292
Ferg Smyth <fergsmyth@gmail.com>
2020-07-03 18:38:10 +0100

    Adding kotlin wrapper

---
+++ src/main/kotlin/org/fergs/bouldering/problems/ProblemsApplication.kt
@@ -0,0 +1,11 @@
+package org.fergs.bouldering.problems
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProblemsApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProblemsApplication>(*args)
+}
---
+++ src/test/kotlin/org/fergs/bouldering/problems/ProblemsApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.fergs.bouldering.problems
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProblemsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
